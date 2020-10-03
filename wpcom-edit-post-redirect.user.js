0fa7f3370bce338ad721d08e678b28dc42306efb
tPenguinLTG <tpenguinltg@disroot.org>
2020-10-02 21:40:40 -0400

    Clean up code
    Reformat and do some light refactoring.
    
    Formatting changes include:
    - Adding padding spaces. This makes the code look less cramped.
    - Removing unnecessary comments
    - Move call to redirectToClassic to the top to take advantage of
      function declaration hoisting.

--- wpcom-edit-post-redirect.user.js
+++ wpcom-edit-post-redirect.user.js
@@ -18,20 +18,23 @@
 // ==/UserScript==
 
 // if already at the classic editor, don't do anything
-if(/classic-editor/.test(window.location.search)) return;
+if (/classic-editor/.test(window.location.search)) return;
 
 // if already at old dashboard URL, redirect to classic editor
-if(/wp-admin/.test(window.location.pathname)) {
+if (/wp-admin/.test(window.location.pathname)) {
   window.location.replace(window.location.href + (window.location.search ? "&" : "?") + "classic-editor");
   return;
 }
 
 
 // gather information from URL
-var parsedUrl=window.location.pathname.match(/(post|page)(?:\/([^\/]+)(?:\/(\d+|new)?)?)?/);
-var postType=parsedUrl[1];
-var blogid=parsedUrl[2];
-var postid=parsedUrl[3];
+var parsedUrl = window.location.pathname.match(/(post|page)(?:\/([^\/]+)(?:\/(\d+|new)?)?)?/);
+var postType = parsedUrl[1];
+var blogid = parsedUrl[2];
+var postid = parsedUrl[3];
+
+// initiate redirect
+redirectToClassic();
 
 
 /**
@@ -39,16 +42,14 @@ var postid=parsedUrl[3];
  */
 function redirectToClassic() {
   // if no blog specified
-  if(!blogid) {
+  if (!blogid) {
     guessClassicLink();
-  }// if
-  else {
+  } else {
     // Redirect to post URL based on API results
     // API docs: https://developer.wordpress.com/docs/api/
-    fetchJSONFile("https://public-api.wordpress.com/rest/v1.1/sites/"+blogid, apiRedirect, guessClassicLink);
-  }//end if
-}//end redirectToClassic
-
+    fetchJSONFile("https://public-api.wordpress.com/rest/v1.1/sites/" + blogid, apiRedirect, guessClassicLink);
+  }
+}
 
 /**
  * Handles the API request via AJAX.
@@ -65,34 +66,31 @@ function fetchJSONFile(path, callback, fallback) {
             if (httpRequest.status === 200) {
                 var data = JSON.parse(httpRequest.responseText);
                 if (callback) callback(data);
-            }//if
-            else {
-                if(fallback) fallback();
-            }//end if
-        }//end if
-    };//end onreadystatechange()
+            } else {
+                if (fallback) fallback();
+            }
+        }
+    };
     httpRequest.open('GET', path);
     httpRequest.send(); 
-}//end fetchJSONFile
-
+}
 
 /**
  * Builds the classic editor link using the specified base.
  * The base should not have a trailing slash.
+ *
+ * @return the URL of the classic editor for the current post ID
  */
 function buildClassicLink(base) {
-    var posturl=base;
-    //new post
-    if(postid == "new" || postid == null) {
-      posturl+="/wp-admin/post-new.php?post_type="+postType+"&classic-editor"
-    }//if
-    //existing post
-    else {
-      posturl+="/wp-admin/post.php?post="+postid+"&action=edit&classic-editor";
-    }//end if
+    var posturl = base;
+    if (postid == "new" || postid == null) { // new post
+      posturl += "/wp-admin/post-new.php?post_type=" + postType + "&classic-editor"
+    } else { // existing post
+      posturl += "/wp-admin/post.php?post=" + postid + "&action=edit&classic-editor";
+    }
 
     return posturl;
-}//end buildLink
+}
 
 /**
  * Guesses the link to the classic editor based on the blog ID, which
@@ -100,38 +98,36 @@ function buildClassicLink(base) {
  */
 function guessClassicLink() {
   // if the blog ID is not numeric, then it is a domain
-  if(blogid && isNaN(blogid)) {
+  if (blogid && isNaN(blogid)) {
     // use the blogid to build URL
     // and redirect
-    window.location.replace(buildClassicLink("https://"+blogid));
-  }//if
-  else {
+    window.location.replace(buildClassicLink("https://" + blogid));
+  } else {
     scrapeSiteLink();
-  }//end if
-}//end guessClassicLink
-
+  }
+}
 
 /**
  * Scrapes the page for the site link
  */
 function scrapeSiteLink() {
-  window.onload=function() {
+  window.onload = function() {
     // FIXME this will not work for Jetpack-enabled sites that have
     // different WordPress and site roots
     // e.g. site is accessed at http://example.com/,
     // but admin at http://example.com/wordpress/wp-admin/
-    blogurl=document.querySelector(".site__content[href]").href;
+    blogurl = document.querySelector(".site__content[href]").href;
 
     // strip trailing slash
-    if(blogurl.charAt(blogurl.length-1) === '/') {
-      blogurl=blogurl.substr(0, blogurl.length-1);
-    }//end if
+    if (blogurl.charAt(blogurl.length-1) === '/') {
+      blogurl = blogurl.substr(0, blogurl.length - 1);
+    }
 
     // in case nothing is returned, don't do anything
-    if(blogurl) {
+    if (blogurl) {
       window.location.replace(buildClassicLink(blogurl));
-    }//end if
-  };//end window.onload
+    }
+  };
 }
 
 /**
@@ -139,17 +135,9 @@ function scrapeSiteLink() {
  * @param data  the parsed API results as a JSON object
  */
 function apiRedirect(data) {
-  // if not a private blog, redirect using API
-  if(!data.error) {
-    //redirect
-    window.location.replace(buildClassicLink(data.URL));
-  }//if
-
-  // else this is a private blog
-  else {
+  if (data.error) { // private blog
     guessClassicLink();
-  }//end if
-}//end apiRedirect
-
-// initiate redirect
-redirectToClassic();
+  } else {
+    window.location.replace(buildClassicLink(data.URL));
+  }
+}
